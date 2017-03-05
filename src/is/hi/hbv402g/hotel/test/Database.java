13098f926797e3ec3821cc17c7b0796044a03565
Ragnheiður Ásta <rak4@hi.is>
2017-03-05 13:07:04 +0000

    Breytt slaufusvigum í Database

--- src/is/hi/hbv402g/hotel/test/Database.java
+++ src/is/hi/hbv402g/hotel/test/Database.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Database {
+public class Database 
+{
     
     private static final String databaseString = "jdbc:sqlite:test.db";
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException 
+    {
         return DriverManager.getConnection(databaseString);
     }
 }
