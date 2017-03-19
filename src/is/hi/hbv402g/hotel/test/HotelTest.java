93257eb9b5953ebdaf21429f14c10d366a963b39
hannesth <hanness91@gmail.com>
2017-03-19 17:24:40 +0000

    Added test path

---
+++ src/is/hi/hbv402g/hotel/test/HotelTest.java
@@ -0,0 +1,154 @@
+package is.hi.hbv402g.hotel.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HotelTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testGetHotelById()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImages()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImages()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAmenities()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAmenities()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStreetAddress()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStreetAddress()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCity()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCity()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPostalCode()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPostalCode()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCountry()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCountry()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStarCount()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStarCount()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReviews()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReviews()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRooms()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRooms()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString()
+	{
+		fail("Not yet implemented");
+	}
+
+}
