2a60672e6109ce9bbc0d0a3606edb3621270d9e1
hannesth <hanness91@gmail.com>
2017-03-22 16:52:37 +0000

    Added MainWindow and MainFrame

---
+++ src/is/hi/hbv402g/hotel/UI/MainWindow.java
@@ -0,0 +1,52 @@
+package is.hi.hbv402g.hotel.UI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainWindow
+{
+
+	private MainFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new MainFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(MainFrame.EXIT_ON_CLOSE);
+	}
+
+}
