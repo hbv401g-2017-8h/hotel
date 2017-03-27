905b4b368dae92ef9984b26999d9d80868dcff83
Viktor Alex <viktoralex@365.is>
2017-03-27 18:15:33 +0000

    Removed unecessary imports.

--- src/is/hi/hbv402g/hotel/models/BookingNight.java
+++ src/is/hi/hbv402g/hotel/models/BookingNight.java
@@ -1,13 +1,7 @@
 package is.hi.hbv402g.hotel.models;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-
-import is.hi.hbv402g.hotel.db.Database;
 
 public class BookingNight
 {
