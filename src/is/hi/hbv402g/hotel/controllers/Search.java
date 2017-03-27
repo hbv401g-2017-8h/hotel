17ef4f32535fa03f54eb67f67d0c4778e0afddd6
hannesth <hanness91@gmail.com>
2017-03-27 17:59:00 +0000

    Added search attributes

--- src/is/hi/hbv402g/hotel/controllers/Search.java
+++ src/is/hi/hbv402g/hotel/controllers/Search.java
@@ -1,12 +1,32 @@
 package is.hi.hbv402g.hotel.controllers;
 
 import java.util.ArrayList;
+import java.util.Date;
+
 import is.hi.hbv402g.hotel.models.Room;
 
 public class Search
 {
 	private ArrayList<Room> availableRooms;
+	private ArrayList<Room> filteredRooms;
 	private String hotelName;
+	private String hotelStreetAddress;
+	private String hotelCity;
+	private String hotelPostalCode;
+	private String hotelCountry;
+	private Date availabilityFrom;
+	private Date availabilityTo;
+	private ArrayList<String> amenities;
+	private int minimumSingleBeds;
+	private int maximumSingleBeds;
+	private int minimumDoubleBeds;
+	private int maximumDoubleBeds;
+	private int minimumPrice;
+	private int maximumPrice;
+	private int minimumStarCount;
+	private int maximumStarCount;
+	private int enSuiteBathroom;
+	private boolean attributesChanged;
 	
 	public void setHotelName(String name)
 	{
