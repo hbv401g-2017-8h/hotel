956811521a06b8615d15ea8d59f2a83a1279c8ce
hannesth <hanness91@gmail.com>
2017-04-05 21:40:16 +0000

    Added ghost text for search text fields

--- src/is/hi/hbv402g/hotel/UI/MainFrame.java
+++ src/is/hi/hbv402g/hotel/UI/MainFrame.java
@@ -23,6 +23,22 @@ public class MainFrame extends JFrame
 	private JTextField textFieldDateTo;
 	
 
+	// Lætur texta hverfa í leitarglugga
+    private void hotelFocusGained(java.awt.event.FocusEvent evt, JTextField tf, String text) {                                         
+        if (tf.getText().equals(text))
+        {
+        	tf.setText("");
+        }
+    }                                        
+
+    // Lætur texta birtast í leitarglugga
+    private void hotelFocusLost(java.awt.event.FocusEvent evt, JTextField tf, String text) {                                       
+        if (tf.getText().equals(""))
+        {
+        	tf.setText(text);
+        }
+    }
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,28 +79,55 @@ public class MainFrame extends JFrame
 		// Define search text fields with input hint text.
 		textFieldHotel = new JTextField();
 		panel.add(textFieldHotel);
-		textFieldHotel.setColumns(15);
-		textFieldHotel.setText("Hotel Name");
-		//textFieldHotel.addFocusListener(new java.awt.event.FocusAdapter() {
-        //    public void focusGained(java.awt.event.FocusEvent evt) {
-                //leitargluggiFocusGained(evt);
-        //    }
-        //    public void focusLost(java.awt.event.FocusEvent evt) {
-                //leitargluggiFocusLost(evt);
-        //    }
-        //});
+		textFieldHotel.setColumns(20);
+		textFieldHotel.setText("Hotel");
+		textFieldHotel.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                hotelFocusGained(evt, textFieldHotel, "Hotel");
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                hotelFocusLost(evt, textFieldHotel, "Hotel");
+            }
+        });
 		
 		textFieldArea = new JTextField();
 		panel.add(textFieldArea);
-		textFieldArea.setColumns(15);
+		textFieldArea.setColumns(20);
+		textFieldArea.setText("Location");
+		textFieldArea.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                hotelFocusGained(evt, textFieldArea, "Location");
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                hotelFocusLost(evt, textFieldArea, "Location");
+            }
+        });
 		
 		textFieldDateFrom = new JTextField();
 		panel.add(textFieldDateFrom);
-		textFieldDateFrom.setColumns(10);
+		textFieldDateFrom.setColumns(15);
+		textFieldDateFrom.setText("From: DD/MM/YYYY");
+		textFieldDateFrom.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                hotelFocusGained(evt, textFieldDateFrom, "From: DD/MM/YYYY");
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                hotelFocusLost(evt, textFieldDateFrom, "From: DD/MM/YYYY");
+            }
+        });
 		
 		textFieldDateTo = new JTextField();
 		panel.add(textFieldDateTo);
-		textFieldDateTo.setColumns(10);
+		textFieldDateTo.setColumns(15);
+		textFieldDateTo.setText("To: DD/MM/YYYY");
+		textFieldDateTo.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                hotelFocusGained(evt, textFieldDateTo, "To: DD/MM/YYYY");
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                hotelFocusLost(evt, textFieldDateTo, "To: DD/MM/YYYY");
+            }
+        });
 		
 		JButton btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
