4f69f3878c6b2d00fead07b53eb13e5ce67c2280
hannesth <hanness91@gmail.com>
2017-04-12 22:29:14 +0000

    Added methods and attributes

--- src/is/hi/hbv402g/hotel/controllers/BookingManager.java
+++ src/is/hi/hbv402g/hotel/controllers/BookingManager.java
@@ -1,6 +1,75 @@
 package is.hi.hbv402g.hotel.controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import is.hi.hbv402g.hotel.db.IDataManager;
+import is.hi.hbv402g.hotel.models.BookingNight;
+import is.hi.hbv402g.hotel.models.Guest;
+import is.hi.hbv402g.hotel.models.Payment;
+
 public class BookingManager
 {
+	private Date dateFrom;
+	private Date dateTo;
+	private ArrayList<BookingNight> bookingNights;
+	private Guest guest;
+	private Payment payment;
+	
+	public Date getDateFrom()
+	{
+		return dateFrom;
+	}
+	public void setDateFrom(Date dateFrom)
+	{
+		this.dateFrom = dateFrom;
+	}
+	public Date getDateTo()
+	{
+		return dateTo;
+	}
+	public void setDateTo(Date dateTo)
+	{
+		this.dateTo = dateTo;
+	}
+	public ArrayList<BookingNight> getBookingNights()
+	{
+		return bookingNights;
+	}
+	public void setBookingNights(ArrayList<BookingNight> bookingNights)
+	{
+		this.bookingNights = bookingNights;
+	}
+	public Guest getGuest()
+	{
+		return guest;
+	}
+	public void setGuest(Guest guest)
+	{
+		this.guest = guest;
+	}
+	public Payment getPayment()
+	{
+		return payment;
+	}
+	public void setPayment(Payment payment)
+	{
+		this.payment = payment;
+	}
+	
+	BookingManager(IDataManager db)
+	{
+		
+	}
+	
+	public void cancel(ArrayList<BookingNight> bookingNights)
+	{
+		
+	}
+	
+	public ArrayList<BookingNight> book(Guest guest, Payment payment, Date dateFrom, Date dateTo)
+	{
+		return bookingNights;
+	}
 
 }
--- src/is/hi/hbv402g/hotel/models/Payment.java
+++ src/is/hi/hbv402g/hotel/models/Payment.java
@@ -2,5 +2,48 @@ package is.hi.hbv402g.hotel.models;
 
 public class Payment
 {
+	private int amount;
+	private boolean completed;
+	private boolean canceled;
+	
+	Payment()
+	{
+		
+	}
+
+	public int getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+
+	public boolean isCompleted()
+	{
+		return completed;
+	}
+
+	public void setCompleted(boolean completed)
+	{
+		this.completed = completed;
+	}
+
+	public boolean isCanceled()
+	{
+		return canceled;
+	}
+
+	public void setCanceled(boolean canceled)
+	{
+		this.canceled = canceled;
+	}
+	
+	public void cancel()
+	{
+		
+	}
 
 }
