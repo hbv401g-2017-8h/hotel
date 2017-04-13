136777870f980bb14cf79ae8c7c6b6792c062f46
hannesth <hanness91@gmail.com>
2017-04-13 18:31:38 +0000

    Added Hotel and Review panel

--- src/is/hi/hbv402g/hotel/UI/HotelPanel.java
+++ src/is/hi/hbv402g/hotel/UI/HotelPanel.java
@@ -11,6 +11,8 @@ public class HotelPanel extends JPanel
 	
 	private JTable table;
 	private DefaultTableModel tableModel;
+	private JTable table_2;
+	private DefaultTableModel tableModel_2;
 
 	/**
 	 * Create the panel.
@@ -24,7 +26,7 @@ public class HotelPanel extends JPanel
 		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
 		
 		JScrollPane scrollPane = new JScrollPane();
-		add(scrollPane);
+		infoPanel.add(scrollPane);
 		
 		tableModel = new DefaultTableModel(0, 5);
 		tableModel.setColumnIdentifiers(new String[] { "Hotel", "Star Count", "Street Address", "City Area", "Country" });
@@ -36,6 +38,16 @@ public class HotelPanel extends JPanel
 		JPanel reviewPanel = new JPanel();
 		add(reviewPanel);
 		reviewPanel.setLayout(new BoxLayout(reviewPanel, BoxLayout.Y_AXIS));
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		reviewPanel.add(scrollPane_2);
+		
+		tableModel_2 = new DefaultTableModel(0, 4);
+		tableModel_2.setColumnIdentifiers(new String[] { "Stars", "Review", "Guest", "Date of Submission" });
+		table_2 = new JTable();
+		table_2.setModel( tableModel_2);
+		
+		scrollPane_2.setViewportView(table_2);
 
 	}
 
