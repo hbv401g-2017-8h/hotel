b383c43375d9e7d7836aac85b130553c4526e242
Viktor Alex <viktoralex@365.is>
2017-04-14 14:28:49 +0000

    Database manager can find rooms by hotel name, hotel location, and availability.

--- src/is/hi/hbv402g/hotel/Application.java
+++ src/is/hi/hbv402g/hotel/Application.java
@@ -2,6 +2,7 @@ package is.hi.hbv402g.hotel;
 
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import is.hi.hbv402g.hotel.db.DatabaseManager;
 import is.hi.hbv402g.hotel.db.IDataManager;
@@ -14,8 +15,16 @@ public class Application
 	public static void main(String[] args)
 	{
 		IDataManager dataManager = new DatabaseManager();
-		//ArrayList<Room> rooms = dataManager.findHotelRooms("saga", "eykjavík", new Date(2017, 4, 12), new Date(2017, 4, 12));
-		ArrayList<Room> rooms = dataManager.findHotelRooms("saga", "eykjavík", new Date(2017, 4, 15), new Date(2017, 4, 17));
+		
+		Calendar calFrom = Calendar.getInstance();
+		calFrom.set(2017, Calendar.APRIL, 25);
+
+		Calendar calTo = Calendar.getInstance();
+		calTo.set(2017, Calendar.MAY, 5);
+		
+		//ArrayList<Room> rooms = dataManager.findHotelRooms("grand", "eykjavík", new Date(2017, 4, 12), new Date(2017, 4, 12));
+		ArrayList<Room> rooms = dataManager.findHotelRooms("grand", "eykjavík", calFrom.getTime(), calTo.getTime());
+		//ArrayList<Room> rooms = dataManager.findHotelRooms("grand", "eykjavík", null, null);
 		
 		for (Room r : rooms)
 		{
